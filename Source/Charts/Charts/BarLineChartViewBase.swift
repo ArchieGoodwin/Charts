df30baf45d3412e0c3e7affe0a3e9081a5d44ff3
Sergey Dikarev <nerowolfe@me.com>
2021-03-23 10:30:13 +0300

    Update BarLineChartViewBase.swift

--- Source/Charts/Charts/BarLineChartViewBase.swift
+++ Source/Charts/Charts/BarLineChartViewBase.swift
@@ -785,9 +785,9 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
                 
                 _isDragging = false
                 
-                delegate?.chartViewDidEndPanning?(self)
             }
-            
+             delegate?.chartViewDidEndPanning?(self)
+
             if _outerScrollView !== nil
             {
                 _outerScrollView?.nsuiIsScrollEnabled = true
